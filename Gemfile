190e22a8fccb66af4ea1cf78d550d6ceb816710b
Tom Johnson <hertzz@users.noreply.github.com>
2015-10-13 01:53:32 +1100

    more changes

--- Gemfile
+++ Gemfile
@@ -1,14 +1,18 @@
-source 'https://rubygems.org'
+source "https://rubygems.org"
 
 gem "middleman", "~> 3.4.0"
 gem "middleman-blog", "~> 3.5.3"
 gem "middleman-livereload", "~> 3.1.1"
 
-group :assets do
-  gem "bitters"
-  gem "bourbon"
-  gem "neat"
-end
+# Assets
+gem "middleman-syntax"
+gem "bitters"
+gem "bourbon"
+gem "neat"
+gem "rouge"
+gem "redcarpet", "~> 3.3.3"
+gem "bootstrap-sass", "~> 3.3.5", :require => false
+gem "jquery-rails", :require => false
 
 # For feed.xml.builder
 gem "builder", "~> 3.0"
--- config.rb
+++ config.rb
@@ -1,69 +1,45 @@
-###
-# Blog settings
-###
-
-# Time.zone = "UTC"
+Time.zone = "Australia/Melbourne"
 
 activate :blog do |blog|
-  # This will add a prefix to all links, template references and source paths
-  # blog.prefix = "blog"
+  blog.prefix = "blog"
 
-  # blog.permalink = "{year}/{month}/{day}/{title}.html"
-  # Matcher for blog source files
-  # blog.sources = "{year}-{month}-{day}-{title}.html"
-  # blog.taglink = "tags/{tag}.html"
-  # blog.layout = "layout"
+  blog.permalink = "{year}/{month}/{day}/{title}.html"
+  blog.sources = "{year}-{month}-{day}-{title}.html"
+  blog.taglink = "tags/{tag}.html"
   # blog.summary_separator = /(READMORE)/
-  # blog.summary_length = 250
-  # blog.year_link = "{year}.html"
-  # blog.month_link = "{year}/{month}.html"
-  # blog.day_link = "{year}/{month}/{day}.html"
-  # blog.default_extension = ".md"
+  blog.summary_length = 350
+  blog.year_link = "{year}.html"
+  blog.month_link = "{year}/{month}.html"
+  blog.day_link = "{year}/{month}/{day}.html"
+  blog.default_extension = ".markdown"
 
   blog.tag_template = "tag.html"
   blog.calendar_template = "calendar.html"
 
-  # Enable pagination
-  # blog.paginate = true
-  # blog.per_page = 5
-  # blog.page_link = "page/{num}"
+  # Pagination
+  blog.paginate = true
+  blog.per_page = 5
+  blog.page_link = "page/{num}"
 end
 
 page "/feed.xml", layout: false
 
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", layout: false
-#
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
 set :css_dir, 'assets/css'
 set :js_dir, 'assets/js'
 set :images_dir, 'assets/images'
+
+# Use redcarpet for formatting
+set :markdown_engine, :redcarpet
+set :markdown, :fenced_code_blocks => true, :smartypants => true
+
+# Use kramdown for formatting
+# set :markdown_engine, :kramdown
+
+# Syntax extension
+activate :syntax
+
+# Directory indexes
+activate :directory_indexes
 
 configure :development do
   activate :livereload
